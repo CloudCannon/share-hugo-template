c79c29253b8df155711a3b52c1978f6fe34dbef3
Nathan Kennedy <nathan.kennedy.1997@gmail.com>
2022-07-08 17:07:46 +1200

    added gitignore

--- node_modules/liquidjs/dist/builtin/filters/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './html';
-export * from './math';
-export * from './url';
-export * from './array';
-export * from './date';
-export * from './object';
-export * from './string';
--- node_modules/liquidjs/dist/cache/cache.d.ts
+++
@@ -1,4 +0,0 @@
-export interface Cache<T> {
-    write(key: string, value: T): void | Promise<void>;
-    read(key: string): T | undefined | Promise<T | undefined>;
-}
